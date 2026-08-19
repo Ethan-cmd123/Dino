0ce3c27a1c1826a4145e75f884513d4e7c5f6168
MountainDew <145302595+Ethan-cmd123@users.noreply.github.com>
2026-08-19 14:05:11 +1200

    oral fix

--- src/pages/oral.jsx
+++ src/pages/oral.jsx
@@ -9,8 +9,15 @@ import React, {
 } from 'react'
 import {
   getCurrentUser,
+  getProfile,
+  getCourseProgress,
+  setCourseTopicCompleted,
+  syncUserCredits,
+  spendUserCredits,
+  onAuthStateChange,
+  signOut,
   getGoldMembership,
-} from '../api/credentials'
+} from "../api/credentials";
 
 const LANGUAGES = [
   {
